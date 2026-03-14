eff8766e83b5c23408a1da0d0326fdacdd4a0f00
Connor Milton <miltonc90@gmail.com>
2026-03-13 22:03:55 -0400

    changed logo text
    changed "HTML'er" to "Tester"

--- html_tester.php
+++ html_tester.php
@@ -536,7 +536,7 @@
             <polyline points="16 18 22 12 16 6"></polyline>
             <polyline points="8 6 2 12 8 18"></polyline>
           </svg>
-          <span class="logo-text">HTML'er</span>
+          <span class="logo-text">Tester</span>
         </div>
         <div class="status">
           <div class="status-dot"></div>
